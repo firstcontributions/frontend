ee638e9928fc68663ab3910ddc80d8467f4f5a4c
Gokul TP <tp.gokul@gmail.com>
2022-02-27 18:03:28 +0000

    added github oauth client functions

--- src/lib/server/githubClient.ts
+++ src/lib/server/githubClient.ts
@@ -1,13 +1,30 @@
 
-export const config = {
-    client: {
-      id: process.env.GITHUB_CLIENT_ID!,
-      secret: process.env.GITHUB_CLIENT_SECRET!
-    },
-    auth: {
-      tokenHost: 'https://github.com/login/'
-    }
-  };
   
   // https://github.com/oauth/authorize?client_id=6903aa8547a474fb9202&redirect_uri=http%3A%2F%2Fapi.firstcontributions.com%2Fv1%2Fauth%2Fcallback&response_type=code&state=ba108af9-97f1-11ec-8e19-0242ac1e0106
   // 
+export function GetRedirectURL() {
+  let url = new URL('https://github.com/login/oauth/authorize')
+  url.searchParams.set("client_id", process.env.GITHUB_CLIENT_ID!)
+  url.searchParams.set("redirect_uri", process.env.GITHUB_REDIRECT_URI!)
+  url.searchParams.set("respose_type", "code")
+  return url.toString()
+}
+
+export async function GetAuthToken(code:string) {
+  let body = {
+    "client_id":  process.env.GITHUB_CLIENT_ID!,
+    "client_secret":  process.env.GITHUB_CLIENT_SECRET!,
+    "code": code,
+    "redirect_uri":  process.env.GITHUB_REDIRECT_URI!,
+  }
+  let request = {
+    method: "POST",
+    body : JSON.stringify(body),
+    headers : {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  }
+  const result = await fetch("https://github.com/login/oauth/access_token", request)
+  return result.text()
+}
