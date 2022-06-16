77b840d1c66e92b0b8d50d65e603e5e7a4fcd7c7
Roshan Jossy <roshan.jossy@jina.ai>
2022-06-16 23:08:00 +0200

    remove react-editor-js and use editorjs out of the box

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,146 +1,78 @@
 import EditorJS from '@editorjs/editorjs'; 
-import React from 'react';
-import { createReactEditorJS } from "react-editor-js";
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import Header from '@editorjs/header'
 import Card from '../Card';
 
-import { EDITOR_JS_TOOLS } from "./editorTools";
 
-const ReactEditorJS = createReactEditorJS();
+const DEFAULT_INITIAL_DATA = () => {
+  return {
+    "time": new Date().getTime(),
+    "blocks": [
+      {
+        "type": "header",
+        "data": {
+          "text": "This is my awesome editor!",
+          "level": 1
+        }
+      },
+    ]
+  }
+}
+ 
+const EDITTOR_HOLDER_ID = 'editorjs';
 
 export default function Editor () {
-    if (!window) return (<></>)
+    const [postTitle, setPostTitle] = useState('Enter Title')
+    const ejInstance = useRef();
+    const [editorData, setEditorData] = React.useState(DEFAULT_INITIAL_DATA);
+   
+    useEffect(() => {
+      if (!ejInstance.current) {
+        initEditor();
+      }
+      return () => {
+        ejInstance.current.destroy();
+        ejInstance.current = null;
+      }
+    }, []);
+   
+    const initEditor = () => {
+      const editor = new EditorJS({
+        holder: EDITTOR_HOLDER_ID,
+        logLevel: "ERROR",
+        data: editorData,
+        onReady: () => {
+          ejInstance.current = editor;
+        },
+        onChange: async () => {
+          let content = await this.editorjs.saver.save();
+          // Put your logic here to save this data to your DB
+          setEditorData(content);
+        },
+        autofocus: true,
+        tools: { 
+          header: Header, 
+        }, 
+      });
+    }; 
+    const handleStorySubmit = () => {
+        console.log(postTitle)
+    }
     return (
-        <Card>
-
-        <div className="prose"> 
-                  <ReactEditorJS
-        tools={EDITOR_JS_TOOLS}
-        defaultValue={{
-          time: 1635603431943,
-          blocks: [
-            {
-              id: "sheNwCUP5A",
-              type: "header",
-              data: {
-                text: "Editor.js",
-                level: 2
-              }
-            },
-            {
-              id: "12iM3lqzcm",
-              type: "paragraph",
-              data: {
-                text:
-                  "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text."
-              }
-            },
-            {
-              id: "fvZGuFXHmK",
-              type: "header",
-              data: {
-                text: "Key features",
-                level: 3
-              }
-            },
-            {
-              id: "xnPuiC9Z8M",
-              type: "list",
-              data: {
-                style: "unordered",
-                items: [
-                  "It is a block-styled editor",
-                  "It returns clean data output in JSON",
-                  "Designed to be extendable and pluggable with a simple API"
-                ]
-              }
-            },
-            {
-              id: "-MhwnSs3Dw",
-              type: "header",
-              data: {
-                text: "What does it mean «block-styled editor»",
-                level: 3
-              }
-            },
-            {
-              id: "Ptb9oEioJn",
-              type: "paragraph",
-              data: {
-                text:
-                  'Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class="cdx-marker">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor\'s Core.'
-              }
-            },
-            {
-              id: "-J7nt-Ksnw",
-              type: "paragraph",
-              data: {
-                text:
-                  'There are dozens of <a href="https://github.com/editor-js">ready-to-use Blocks</a> and the <a href="https://editorjs.io/creating-a-block-tool">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games.'
-              }
-            },
-            {
-              id: "SzwhuyoFq6",
-              type: "header",
-              data: {
-                text: "What does it mean clean data output",
-                level: 3
-              }
-            },
-            {
-              id: "x_p-xddPzV",
-              type: "paragraph",
-              data: {
-                text:
-                  "Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below"
-              }
-            },
-            {
-              id: "6W5e6lkub-",
-              type: "paragraph",
-              data: {
-                text:
-                  'Given data can be used as you want: render with HTML for <code class="inline-code">Web clients</code>, render natively for <code class="inline-code">mobile apps</code>, create markup for <code class="inline-code">Facebook Instant Articles</code> or <code class="inline-code">Google AMP</code>, generate an <code class="inline-code">audio version</code> and so on.'
-              }
-            },
-            {
-              id: "eD2kuEfvgm",
-              type: "paragraph",
-              data: {
-                text:
-                  "Clean data is useful to sanitize, validate and process on the backend."
-              }
-            },
-            {
-              id: "N8bOHTfUCN",
-              type: "delimiter",
-              data: {}
-            },
-            {
-              id: "IpKh1dMyC6",
-              type: "paragraph",
-              data: {
-                text:
-                  "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏"
-              }
-            },
-            {
-              id: "FF1iyF3VwN",
-              type: "image",
-              data: {
-                file: {
-                  url: "https://codex.so/public/app/img/external/codex2x.png"
-                },
-                caption: "",
-                withBorder: false,
-                stretched: false,
-                withBackground: false
-              }
-            }
-          ]
-        }}
-      />
-
+        <Card classes="prose">
+        <h2 contentEditable  onInput={(e) => setPostTitle(e.target.textContent)}>{postTitle}</h2>
+        <div> 
+          <div id={EDITTOR_HOLDER_ID}> </div>
         </div>
+        <style jsx>
+            {`
+                [contenteditable]:focus {
+                    outline: 0px solid transparent;
+                    border-bottom: 1px dashed #aaa;
+                }
+            `}
+        </style>
+        <button onClick={() => handleStorySubmit()}>submit </button>
         </Card>
     )
 
