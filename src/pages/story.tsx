cd28a967e57274ec6735191e25ffdc8a8c7cf709
Gokul TP <tp.gokul@gmail.com>
2022-06-18 18:08:07 +0100

    story changes

--- src/pages/story.tsx
+++ src/pages/story.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic'
 import Layout from '../components/Layout'
 
-const Editor = dynamic(() => import('../components/editor/StoryEditor'))
+const Editor = dynamic(() => import('../components/story/StoryEditor'))
 export default function Story () {
     return (
         <div>
