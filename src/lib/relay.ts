e8e724e7265598a70fabdf25439e795422aba281
Roshan Jossy <roshan.jossy@jina.ai>
2022-06-18 18:25:22 +0200

    fix type errors

--- src/lib/relay.ts
+++ src/lib/relay.ts
@@ -1,14 +1,20 @@
-import { withHydrateDatetime } from 'relay-nextjs/date';
-import { GraphQLResponse } from 'relay-runtime';
+import { NextPageContext } from 'next'
+import { withHydrateDatetime } from 'relay-nextjs/date'
+import { GraphQLResponse } from 'relay-runtime'
 
+type Params = {
+  text: string | null | undefined
+}
 
-export  async function fetchGraphQL (params, variables) {
+type QueryVars = Record<any, any>
+
+export async function fetchGraphQL(params: Params, variables: QueryVars) {
   const results = await fetch('http://api.firstcontributions.com/v1/graphql', {
     method: 'POST',
     credentials: 'include',
     headers: {
       Accept: 'application/json',
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       query: params.text,
@@ -16,40 +22,35 @@ export  async function fetchGraphQL (params, variables) {
     }),
   })
 
-  const text = await results.text()    
-  const data = JSON.parse(
-    text,
-    withHydrateDatetime
-  ) as GraphQLResponse;
+  const text = await results.text()
+  const data = JSON.parse(text, withHydrateDatetime) as GraphQLResponse
 
-  return data;
+  return data
 }
 
+export const makeFetchGraphql = (ctx: NextPageContext) => {
+  return async function fetchGraphQL(params: Params, variables: QueryVars) {
+    const results = await fetch(
+      'http://api.firstcontributions.com/v1/graphql',
+      {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Cookie: ctx.req ? ctx.req.headers.cookie! : '',
+        },
+        body: JSON.stringify({
+          query: params.text,
+          variables,
+        }),
+      }
+    )
 
-export const makeFetchGraphql=(ctx) => {
-  return async function fetchGraphQL (params, variables) {
+    const text = await results.text()
+    console.log('response', text)
+    const data = JSON.parse(text, withHydrateDatetime) as GraphQLResponse
 
-    const results = await fetch('http://api.firstcontributions.com/v1/graphql', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        Accept: 'application/json',
-        "Content-Type": "application/json",
-        Cookie: ctx?.req.headers.cookie,
-      },
-      body: JSON.stringify({
-        query: params.text,
-        variables,
-      }),
-    })
-    
-    const text = await results.text()  
-    console.log("response", text)
-    const data = JSON.parse(
-      text,
-      withHydrateDatetime
-    ) as GraphQLResponse;
-  
-    return data;
+    return data
   }
 }
--- src/lib/server/server_environment.ts
+++ src/lib/server/server_environment.ts
@@ -1,23 +1,21 @@
-import { graphql } from 'graphql';
-import { withHydrateDatetime } from 'relay-nextjs/date';
-import { GraphQLResponse, Network, Environment, Store, RecordSource } from 'relay-runtime';
-import { fetchGraphQL, makeFetchGraphql } from '../relay';
+import { NextPageContext } from 'next'
+import { Network, Environment, Store, RecordSource } from 'relay-runtime'
+import { makeFetchGraphql } from '../relay'
 
 // Relay is not prescriptive about how GraphQL requests are made.
 // This is an example showing how to request GraphQL data.
 // You should fill this in with how to make requests to your GraphQL
 // API of choice.
 
-export function createServerNetwork(ctx) {
-  return Network.create(makeFetchGraphql(ctx));
+export function createServerNetwork(ctx: NextPageContext) {
+  return Network.create(makeFetchGraphql(ctx))
 }
 
 // Optional: this function can take a token used for authentication and pass it into `createServerNetwork`.
-export function createServerEnvironment(ctx) {
-
+export function createServerEnvironment(ctx: NextPageContext) {
   return new Environment({
     network: createServerNetwork(ctx),
     store: new Store(new RecordSource()),
     isServer: true,
-  });
+  })
 }
