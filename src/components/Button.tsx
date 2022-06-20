14d4ed7f6d4fee92cec3f54ede754bbcb469814b
Roshan Jossy <roshan.jossy@jina.ai>
2022-06-21 00:47:36 +0200

    update navbar

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ const Button = ({ children, onClick: onClick }: ButtonProps) => {
   return (
     <button
       type="button"
-      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      className="text-white font-bold bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300  rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       onClick={onClick}
     >
       {children}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,31 @@
-export default function Navbar () {
-    return( <div className="flex fixed p-4 w-full bg-blue-400 top-0 z-10">
-        Logo
-        <div>bu</div>
-    </div>)
+import { useRouter } from 'next/router'
+import React from 'react'
+import Button from '../Button'
+
+export default function Navbar() {
+  const router = useRouter()
+  const { pathname } = router
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10 w-full h-16 bg-white p-3 shadow-sm">
+      <div className="container m-auto flex justify-between">
+        <a href="/">
+          <div className="logo w-10 h-10"></div>
+        </a>
+        {pathname !== '/story' && (
+          <Button>
+            <a href="/story">
+              <a>Create Post </a>
+            </a>
+          </Button>
+        )}
+      </div>
+      <style jsx>{`
+        .logo {
+          background: url('https://avatars1.githubusercontent.com/u/65761570?s=460&u=640f39b808c75c6b86460aa907dd030bcca2f3c7&v=4');
+          background-repeat: no-repeat;
+          background-size: contain;
+        }
+      `}</style>
+    </nav>
+  )
 }
