e304ca32cef6bc41100ccb1d7b108cde8000bd3b
Roshan Jossy <roshan.jossy@jina.ai>
2022-07-03 15:14:40 +0200

    use userdetails in user profile page

--- src/pages/users/[handle].tsx
+++ src/pages/users/[handle].tsx
@@ -1,10 +1,55 @@
 import { useRouter } from 'next/router'
+import { NextPageContext } from 'next'
+import { graphql, usePreloadedQuery } from 'react-relay'
+import { RelayProps, withRelay } from 'relay-nextjs'
+import Layout from '../../components/Layout'
+import { getClientEnvironment } from '../../lib/client_environment'
+import Card from '../../components/Card'
+import UserDetails from '../../components/UserDetails/UserDetails'
+import { pages_UserQuery } from '../../queries/__generated__/pages_UserQuery.graphql'
+import RelayModernEnvironment from 'relay-runtime/lib/store/RelayModernEnvironment'
+import { UserDetails_user$key } from '../../queries/__generated__/UserDetails_user.graphql'
 
-const User = () => {
+const UserQuery = graphql`
+  query pages_UserQuery {
+    viewer {
+      handle
+      ...UserDetails_user
+    }
+  }
+`
+
+const User = ({
+  preloadedQuery,
+}: RelayProps<Record<string, never>, pages_UserQuery>) => {
+  const query = usePreloadedQuery(UserQuery, preloadedQuery)
   const router = useRouter()
   const { handle } = router.query
 
-  return <h1>Welcome {handle}</h1>
+  return (
+    <div>
+      <Layout
+        sidebarContentRight={handle}
+        sidebarContentLeft={
+          <Card>
+            <UserDetails user={query.viewer as UserDetails_user$key} />
+          </Card>
+        }
+      >
+        {handle}
+      </Layout>
+    </div>
+  )
 }
 
-export default User
+export default withRelay(User, UserQuery, {
+  createClientEnvironment: () =>
+    getClientEnvironment() as RelayModernEnvironment,
+  createServerEnvironment: async (ctx: NextPageContext) => {
+    const { createServerEnvironment } = await import(
+      '../../lib/server/server_environment'
+    )
+
+    return createServerEnvironment(ctx)
+  },
+})
