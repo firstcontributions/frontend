6e2f765ec4c8e752c3a5e0f544621df1bde1baf1
Roshan Jossy <roshanjossey@gmail.com>
2022-09-30 01:37:16 +0200

    change theme button to use sun, moon images

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,31 @@ import { ReactNode } from 'react'
 type ButtonProps = {
   children: ReactNode
   onClick?: () => void
+  color?: 'primary' | 'secondary' | 'transparent'
+  variant?: 'contained' | 'outlined' | 'text'
 }
 
-const Button = ({ children, onClick: onClick }: ButtonProps) => {
+const Button = ({
+  children,
+  onClick: onClick,
+  color = 'primary',
+  variant = 'contained',
+}: ButtonProps) => {
+  const getColorStyles = (color: string) => {
+    if (color === 'primary') {
+      return 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+    }
+    if (color === 'transparent') {
+      return 'focus:ring-transparent'
+    }
+    return ''
+  }
   return (
     <button
       type="button"
-      className="text-white font-bold bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300  rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      className={` focus:ring-4 rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ${getColorStyles(
+        color
+      )}  `}
       onClick={onClick}
     >
       {children}
