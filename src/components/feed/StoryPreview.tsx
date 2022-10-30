a8d39184ab4fc4ea641e9a653a27ea375b7c792f
Roshan Jossy <roshanjossey@gmail.com>
2022-10-30 23:01:42 +0100

    move links to parents in story preview and user snippet

--- src/components/feed/StoryPreview.tsx
+++ src/components/feed/StoryPreview.tsx
@@ -33,27 +33,27 @@ const StoryPreview = ({ story }: StoryPreviewProps) => {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <div className="my-8 mr-2 flex flex-col bg-white  dark:bg-dark-700 rounded-md">
-        <div>
-          {data.thumbnail ? (
-            <img className="cover-image" src={data.thumbnail} alt="" />
-          ) : (
-            <div className="image-placeholder bg-gray-100 dark:bg-dark-500"></div>
-          )}
-        </div>
-        <div className="px-4 pb-4">
-          <div className="flex flex-row justify-between">
-            <UserSnippet user={data.createdBy} />
-            <GoKebabVertical className="mt-4" />
-          </div>
-          <div className="prose dark:text-gray-100">
-            <Link href={`/story/${data.id}___${data.urlSuffix}`}>
-              <a>
+        <Link href={`/story/${data.id}___${data.urlSuffix}`}>
+          <a>
+            <div>
+              {data.thumbnail ? (
+                <img className="cover-image" src={data.thumbnail} alt="" />
+              ) : (
+                <div className="image-placeholder bg-gray-100 dark:bg-dark-500"></div>
+              )}
+            </div>
+            <div className="px-4 pb-4">
+              <div className="flex flex-row justify-between">
+                <UserSnippet user={data.createdBy} />
+                <GoKebabVertical className="mt-4" />
+              </div>
+              <div className="prose dark:text-gray-100">
                 <h3 className="dark:text-gray-200">{data.title}</h3>
-              </a>
-            </Link>
-            <p className="abstract-content">{data.abstractContent}</p>
-          </div>
-        </div>
+                <p className="abstract-content">{data.abstractContent}</p>
+              </div>
+            </div>
+          </a>
+        </Link>
         <StoryPreviewFooter story={data} />
         <style jsx>
           {`
