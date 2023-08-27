544208694387c394b9a9c32651d3b4fe766dcdc8
Roshan Jossy <roshanjossey@gmail.com>
2023-08-27 21:02:40 +0200

    add styles for typography

--- src/components/story/PostEditor.tsx
+++ src/components/story/PostEditor.tsx
@@ -9,7 +9,7 @@ const Tiptap = () => {
   })
 
   return (
-    <div className="relative min-h-[500px] w-full max-w-screen-lg border-stone-200 bg-white  dark:text-gray-300 dark:bg-dark-600 p-12 px-8 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg">
+    <div className="relative min-h-[500px] w-full max-w-screen-lg border-stone-200 bg-white  dark:text-gray-300 dark:bg-dark-600 p-12 px-8 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg prose dark:prose-invert">
       <EditorContent editor={editor} />
     </div>
   )
